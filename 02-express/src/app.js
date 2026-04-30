3f16bb1f33699d40997cfa8322c058fa8b4f0e72
Andi Ahmad Yusup <andiahmadyusup@gmail.com>
2026-04-30 15:18:37 +0700

    refactor(server): remove morgan and cors middleware

--- 02-express/src/app.js
+++ 02-express/src/app.js
@@ -1,6 +1,4 @@
 import express from "express";
-import morgan from "morgan";
-import cors from "cors";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import { config } from "./config.js";
@@ -10,14 +8,12 @@ import { notFound, errorHandler } from "./middlewares/error.middleware.js";
 dotenv.config();
 const app = express();
 
-app.use(cors());
-app.use(morgan("dev"));
 app.use(express.json());
 
 app.get("/", (req, res) => res.json({ message: "API is running" }));
 app.use("/api/products", productRoutes);
 
-// Error handling middleware (after routes)
+
 app.use(notFound);
 app.use(errorHandler);
 
